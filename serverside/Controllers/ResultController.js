6186c8255527996071905e52c1b0058d9212850b
Jace Grant <jacegrant@hotmail.com>
2022-09-05 00:36:52 +1000

    [saving-scores] Separating out user auth functionality and adding create result endpoint.

--- serverside/Controllers/ResultController.js
+++ serverside/Controllers/ResultController.js
@@ -15,55 +15,109 @@ const bcryptjs = require('bcryptjs');
  * then creating the new record in the database.
  */
 router.post('/', async (req, res) => {
-    // Retrieve JWT from the request headers
-    const token = req.headers.auth; 
 
-    // Verify that the JWT has the correct signature. Return with an unauthorised response if invalid
-    try {
-        if(!jwt.verify(token, 'supersecret')) { // TODO: change supersecret to EnvVar
-            console.log('Invalid JWT');
-            res.status(401).json({error: "Invalid Credentials (Invalid JWT). Please try signing in again"});
-            return;
-        }
-    } catch (err) {
-        res.status(401).json({error: `Error thrown verifying token: ${err}`});
-        console.log(err);
-        return;
-    }
-    
-    // Decode Token once it has been verified.
-    const tokenDecoded = jwt.decode(token); 
-
-    // Attempt to find a user with the details in the token sent.
-    const user = await User.findOne({
-        email: tokenDecoded.email,
-    })
-
-    // If no user exists then respond with an error response.
-    if(user == null) {
-        res.status(401).json({error: "Invalid Credentials (Unable to find user). Please try signing in again"});
+    // Determine the logged in user that has sent this request.
+    const tokenDecoded = findUser(req);
+    if(tokenDecoded === null) {
+        res.status(401).json({
+            message: "User authentication error. Please sign in again."
+        });
         return;
     }
 
     // Create a new results with the given user and day and number of attempts. 
     try {
         await Result.create({
-            userEmail: user.email,
+            userEmail: tokenDecoded.email,
             wordleDay: req.body.day,
             numAttempts: req.body.attempts,
         });
     } catch (err) {
         console.log(`Failed to store result due to: ${err}`);
         res.status(500).json({
-            status: "Unable to store result"
+            message: "Unable to store result"
         });
         return;
     }
 
 
     res.status(200).json({
-        status: "Result Recorded"
+        message: "Result Recorded"
     });
 });
 
+/**
+ * Requesting to determine if a user has attempted a particular days puzzle. 
+ * 
+ * If the user has attempted the provided days puzzle, the number of attempts will be returned. 
+ * Else, 0 will be returned for NoScore.
+ */
+router.get('/:day', async (req, res) => {
+
+    // Determine the logged in user that has sent this request.
+    const tokenDecoded = findUser(req);
+    if(tokenDecoded === null) {
+        res.status(401).json({
+            message: "User authentication error. Please sign in again."
+        });
+        return;
+    }
+
+    // Attempt to find a result with the details in the request.
+    const score = await Result.findOne({
+        userEmail: tokenDecoded.email,
+        wordleDay: req.params.day,
+    })
+
+    // If no result exists then respond with no score recorded response.
+    if(score == null) {
+        res.status(200).json({
+            result: 0
+        });
+        return;
+    }
+
+    res.status(200).json({
+        result: score.numAttempts
+    })
+})
+
+/**
+ * Verifying that a bearer JWT token with the correct signature is in the request headers.
+ * If valid and verified, the JWT is decoded and returned. If any error  then null is returned.
+ * 
+ * @param req The request object from the controller endpoint parameter.
+ * @returns Decoded JWT if verified and validated. Null otherwise.
+ */
+const findUser = (req) => {
+    
+    // Check if the token was sent
+    if(req.headers.authorization == null) {
+        console.log("No bearer token sent with request.")
+        return null;
+    }
+
+    // Retrieve bearer token
+    let token;
+    try {
+        token = req.headers.authorization.split(" ")[1];
+    } catch (err) {
+        console.log(`Error with token format: ${err}`);
+        return null;
+    }
+
+    // Verify the token signature, returning null if invalid signature.
+    try {
+        if(!jwt.verify(token, 'supersecret')) { // TODO: change supersecret to EnvVar
+            console.log('Invalid JWT');
+            return null;
+        }
+    } catch (err) {
+        console.log(err);
+        return null;
+    }
+
+    // Decode Token once it has been verified and return from function.
+    return jwt.decode(token); 
+}
 module.exports = router;
